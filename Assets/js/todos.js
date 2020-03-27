e6e41f82b0247b9ad1cf1c53e3904132d0aaa792
Leah Riffell <leah.riffell@gmail.com>
2020-03-27 09:07:17 +0900

    Add delete and mark complete

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,3 +1,20 @@
 $( ".checkboxAndTask").click(function() {
-  $(this).toggleClass("task--completed")
+  $(this).parent().find(".task").toggleClass("task--completed");
+  $(this).parent().find(".markComplete").toggleClass("fa-square fa-check-square");
 });
+
+$(".delete").click(function() {
+  $(this).parent().fadeOut(300, function() {
+    $(this).remove();
+  })
+});
+
+$("input").click(function() {
+  $(this).val("");
+});
+
+$("input").on("keydown", (function(event) {
+  if(event.which === 13) {
+    alert("hey");
+  }
+}));
