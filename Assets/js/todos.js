604b9c714138c634c674202bc67848636b81afa7
Leah Riffell <leah.riffell@gmail.com>
2020-03-27 10:25:20 +0900

    Add a new todo

--- Assets/js/todos.js
+++ Assets/js/todos.js
@@ -1,20 +1,22 @@
-$( ".checkboxAndTask").click(function() {
+$( "ul").on("click", ".checkboxAndTask", function() {
   $(this).parent().find(".task").toggleClass("task--completed");
   $(this).parent().find(".markComplete").toggleClass("fa-square fa-check-square");
 });
 
-$(".delete").click(function() {
-  $(this).parent().fadeOut(300, function() {
+$("ul").on("click", ".delete", function() {
+  $(this).parent().fadeOut(300, function(event) {
     $(this).remove();
-  })
+  });
 });
 
 $("input").click(function() {
   $(this).val("");
 });
 
-$("input").on("keydown", (function(event) {
+$("input").keypress(function(event) {
   if(event.which === 13) {
-    alert("hey");
+    var todoText = $(this).val();
+    $(this).val("Add a new todo");
+    $("ul").append( " <div class='taskContainer'><div class='checkboxAndTask'><i class='markComplete far fa-square'></i><li class='task'> " + todoText + " </li></div><i class='delete fas fa-trash'></i></div> " );
   }
-}));
+ });
